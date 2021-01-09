dacbac3a35b0e3561f16e8b2cfdda5c389751786
jordannadroj <jordansiemens93@gmail.com>
2021-01-09 15:44:21 +0100

    complete withou stlye

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,50 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    # raise
+  end
+
+  def new
+    @task = Task.new # needed to instantiate the form_for
+  end
+
+  def show
+    # raise
+    @task = Task.find(params[:id])
+    @completed = ''
+    @task.completed ? @completed = 'complete' : @completed = ' not complete'
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    # @task.save
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    # raise
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
+
+
